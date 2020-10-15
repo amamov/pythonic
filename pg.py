3e74eca4356ba720aea07aea5c21371ec05459c3
Yoon sang seok <63184031+amamov@users.noreply.github.com>
2020-10-15 23:57:45 +0900

    commit

--- pg.py
+++ pg.py
@@ -4,4 +4,22 @@
 
 # 리스트 반대로 바꾸기
 # string 반대로 바꾸기
-"".join(["1", "2", "3"])
+
+
+def bulls_and_cows(problem, answer):
+    strike, ball = 0, 0
+
+    for pr_e in problem:  # 5
+        for as_e in answer:  # 1
+            if pr_e == as_e:
+                strike += 1  # 1
+                answer.remove(as_e)
+                break
+            elif as_e in problem:  #
+                ball += 1
+                break
+
+    return [(strike), (ball)]
+
+
+print(bulls_and_cows([1, 5, 8], [1, 8, 9]))
