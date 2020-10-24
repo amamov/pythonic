7c0a93399dfad05ec8afebda69ff60858311be87
Yoon sang seok <63184031+amamov@users.noreply.github.com>
2020-10-24 15:06:16 +0900

    삼항연산자

--- 02_syntax/01_if.py
+++ 02_syntax/01_if.py
@@ -12,3 +12,20 @@ else:
 
 # Enter the number : 3
 # 3  is divisible by 2 or 3.
+
+### 삼항 연산자
+number1 = 1
+number2 = 2
+number3 = 3
+
+# 1. and-or : 인터프리터의 원리를 이용한 문법 -> 오류가 존재할 수 있다. 5==5 and 5-5 or 5+5 -->10(오류)
+result1 = (number2 > number1) and "1은 2보다 작다" or "1은 2보다 크다."
+
+# 2. if-else : 권장되는 삼항 연산자
+result2 = "3은 2보다 크다" if number3 > number2 else "3은 2보다 작다."
+
+print(result1)
+# 1은 2보다 작다
+
+print(result2)
+# 3은 2보다 크다
